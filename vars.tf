6f09491adf1a5a5f14976599b01bf77ae4bd4e88
Chowdary-Hari <chandrahari321@outlook.com>
2025-09-13 13:12:09 +0530

    Automated commit #1552

--- vars.tf
+++ vars.tf
@@ -2,3 +2,10 @@ variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
 variable "subnets" {}
+
+
+# variable "vpc_id" {}
+# variable "cidr_block" {}
+
+# variable "name" {}
+# variable "azs" {}
